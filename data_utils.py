dbf08bcbf54879c99948b80c09af34c6516ef1a2
ritvikareddy18 <ritvikareddy18@ccs.neu.edu>
2018-12-10 12:54:21 -0500

    adding other models

--- data_utils.py
+++ data_utils.py
@@ -2,10 +2,13 @@
 #__date__ = 2018-12-09
 
 import cv2
+import dlib
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+
 from keras.utils import np_utils
+from skimage.feature import hog
 
 
 # takes in the data frame of entire and the type of use(i.e dataset) and return processed data (x,y)
@@ -20,6 +23,38 @@ def process_data(main_df, use):
     y = np_utils.to_categorical(y)
 
     return x, y
+
+
+# takes the 'x' output of process_data as input
+def extract_hog_features(images_array):
+    hog_features_res = []
+    for img in images_array:
+        hog_feature = hog(img, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1))
+        hog_features_res.append(hog_feature)
+    # print(len(hog_features_res), len(hog_features_res[0]))
+    return np.asarray(hog_features_res)
+
+
+# takes the 'x' output of process_data as input
+def extract_landmark_features(images_array, flatten=False):
+    face_landmarks_res = []
+    # load detector and predictors from dlib
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    # since our images are already cropped to contain only the face, we don't need to use a detector
+    rect = [dlib.rectangle(left=1, top=1, right=47, bottom=47)]
+
+    # detect face using openCV i.e cv2
+    for img in images_array:
+        cv2.imwrite('temp.jpg', img)
+        saved_img = cv2.imread('temp.jpg', 0)
+        face_landmarks = predictor(saved_img, rect[0])
+        face_landmarks_pixels = np.array([[point.x, point.y] for point in face_landmarks.parts()])
+        # print(face_landmarks)
+        face_landmarks_res.append(face_landmarks_pixels)
+    if flatten:
+        face_landmarks_res = [res.flatten() for res in face_landmarks_res]
+    return np.asarray(face_landmarks_res)
 
 
 def convert_probs_to_labels(y_probs, labels):
